72b222f68ff6f53f98b51b991d29372207f81d67
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-08 03:52:25 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,16 +2,15 @@ require 'pry'
 
 def reformat_languages(languages)
   output = {}
-  styles = []
+
   languages.each do |style, languages_hsh|
     languages_hsh.each do |language, type_hsh|
       output[language] = {}
-      styles.push(style)
+
       type_hsh.each do |type_key, type|
         output[language][:type] = type
       end
     end
-    output[language][:style] = styles
   end
   binding.pry
   output
