de6d61928c10bc8a78064abfda4518c206661977
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-08 03:07:26 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,6 +2,7 @@ def reformat_languages(languages)
   output = {}
   languages.each do |style, languages_hsh|
     languages_hsh.each do |language, type_hsh|
+if 
     output[language] = {:style => [style]}
       type_hsh.each do |type_key, type|
         output[language][:type] = type
